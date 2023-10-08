a5a25321e4feb110031fc7dea5d376075652bc30
jomivaan <jomivaan@gmail.com>
2023-10-08 11:14:41 +0100

    organization

---
+++ Fase2/solution.py
@@ -0,0 +1,106 @@
+import search
+
+class FleetProblem(search.Problem):    
+    def __init__(self):
+        self.req = [] # Store the requests
+        self.t_opt = {} # Store the optimal transportation times
+        self.vehicles = [] # Store the vehicles
+        self.NP = 0 # Number of all pickup/drop-off points
+        self.NR = 0 # Number of requests
+        self.NV = 0 # Number of vehicles
+        
+    def load(self, fh):
+        ''' Loads a problem from the opened file object fh. '''
+        counter = 0
+        
+        # Read file line by line and store in a list
+        data = fh.readlines()
+        
+        # mode = 0 (do not read data), mode = 1 (read transportation times); 
+        # mode = 2 (read requests), mode = 3 (read vehicles)
+        mode = 0 
+        
+        # Iterate through the input file lines
+        for value in data:
+            v = value.split()
+           
+            # Check if line is relevant 
+            if v != [] and v[0] != "#":
+                if v[0] == 'P': # Detect information about number of points 
+                    self.NP = int(v[1])
+                    mode = 1
+                    counter = 0
+                
+                elif v[0] == 'R': # Detect information about number of requests
+                    self.NR = int(v[1])             
+                    mode = 2
+                    counter = 0
+                    
+                elif v[0] == 'V': # Detect information about number of vehicles
+                    self.NV = int(v[1])
+                    mode = 3
+                    counter = 0
+                    
+                elif mode == 1: # Read transportation times
+                    count_aux = counter + 1
+                    for to in v:  
+                        # Tod: Transportation time from origin (counter) to dropoff (count_aux)
+                        Tod = float(to)
+                        
+                        # T(p, q) > 0 if p ̸= q (como está no enunciado)
+                        if Tod <= 0:
+                            raise Exception("Invalid transportation time")
+                
+                        self.t_opt[(counter, count_aux)] = Tod # (origin, dropoff) = (counter, count_aux)
+                        count_aux += 1
+                    counter += 1
+                    
+                elif mode == 2: # Read requests
+                    req_time = float(v[0])
+                    
+                    # Valid requests have t (request time) ≥ 0 (como está no enunciado)
+                    if req_time < 0:
+                        raise Exception("Invalid request time")
+
+                    self.req.append((req_time, int(v[1]), int(v[2]), int(v[3]))) # (reqtime, origin, drop_off, num_p)
+                    counter += 1
+                    
+                elif mode == 3: # Read vehicles
+                    self.vehicles.append(int(v[0])) # capacity
+                    counter += 1 
+        
+        # Transportation time is 0 if origin == dropoff: T(p, p) = 0  (como está no enunciado)
+        for p in range(self.NP):
+            self.t_opt[(p, p)] = 0
+
+                        
+    def cost(self, sol):
+        ''' Compute cost of solution sol. '''
+        cost = 0         
+    
+        # structure of solution element, i.e, an action: (pic_drop, v_i, r_i, t)
+        # pic_drop: a string, either 'Pickup' or 'Dropoff', with self-evident meaning;
+        # v_i: an integer corresponding to the vehicle index;
+        # r_i: an integer corresponding to the request index;
+        # t: a float corresponding to the time of the action
+        for action in sol:
+            pic_drop, _, r_i, td = action
+            
+            if pic_drop == 'Dropoff':
+                t_req, origin, drop_off, _  = self.req[r_i]
+                
+                # Get Tod of the request
+                if origin > drop_off:
+                    origin, drop_off = drop_off, origin
+                Tod = self.t_opt[(origin, drop_off)]
+                
+                # Delay (dr) = 
+                # = Dropoff time of an action (td) -
+                # - Time of the request of an action (treq) -
+                # - Optimal transportation time between origin and drop_off (Tod)
+                dr = td - t_req - Tod
+                
+                # Cost of sol = sum of the request's delay
+                cost += dr
+                   
+        return cost 
